51cbb6904285f293132d9bacb8787af88283f910
Miguel-AngelGarcia <mgar574@wgu.edu>
2023-03-16 17:29:52 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const scoreCheck = function (playerScore) {
+  if (playerScore >= 100) {
+    //game is over!
+  }
+};
+//[.]Selector is for classes
+//# is for IDs
+const score0Element = document.querySelector('#score--0');
+//a little bit faster than query selector
+const score1Element = document.getElementById('score--1');
+
+score0Element = 0;
+score1Element = 0;
+
+//will hide dice
