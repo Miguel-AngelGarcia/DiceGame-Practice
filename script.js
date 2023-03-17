50f972d12a8c9cb9b1ef2fd6a6fddf283c4dda0b
Miguel-AngelGarcia <mgar574@wgu.edu>
2023-03-17 17:41:24 -0400

    Gonna start working on switching players

--- script.js
+++ script.js
@@ -11,12 +11,18 @@ const score0Element = document.querySelector('#score--0');
 //a little bit faster than query selector
 const score1Element = document.getElementById('score--1');
 const diceElement = document.querySelector('.dice');
-const btnNewGame = document.querySelector('btn--new');
-const btnRollDice = document.querySelector('btn--roll');
-const btnHold = document.querySelector('btn--hold');
+const btnNewGame = document.querySelector('.btn--new');
+const btnRollDice = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0Element = document.getElementById('current--0');
+const current1Element = document.getElementById('current--1');
 
 score0Element.textContent = 0;
 score1Element.textContent = 0;
+//    Player 1 | Player 2
+let scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 //will hide dice
 //will create '.hidden' class in css
 diceElement.classList.add('hidden');
@@ -29,5 +35,18 @@ btnRollDice.addEventListener('click', function () {
 
   const dice = Math.trunc(Math.random() * 6 + 1);
   diceElement.classList.remove('hidden');
-  console.log('hi');
+  diceElement.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    //add number to current score
+    currentScore += dice;
+    current0Element.textContent = currentScore; //TODO
+  } else {
+    currentScore = 0;
+    current0Element.textContent = currentScore;
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  //add to players total score
 });
